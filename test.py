5f866b3e1fa3a67ef9b2c63445dc1d9437693780
ElsSweep <e.sweep@tudelft.nl>
2018-11-07 13:22:19 +0100

    surface area function added

--- test.py
+++ test.py
@@ -8,3 +8,11 @@ print("The radius of the circle is", p)
 def circumference(r):
     """This function calculates the circumference of a circle"""
     return 2*np.pi*r
+
+print("the circumference of the circle is", circumference(p))
+
+def surface_area(r):
+    """ this is a test to see how to combine two versions """
+    return np.pi*r**2
+
+print("The surface area of the circle is", surface_area(p))
