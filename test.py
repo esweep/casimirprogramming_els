097f5b51a934d3f85ede0447fb767b3a24c0c877
ElsSweep <e.sweep@tudelft.nl>
2018-11-07 13:20:34 +0100

    circumference function added

--- test.py
+++ test.py
@@ -1,1 +1,10 @@
 print("hello world")
+
+import numpy as np
+
+p = 5
+print("The radius of the circle is", p)
+
+def circumference(r):
+    """This function calculates the circumference of a circle"""
+    return 2*np.pi*r
