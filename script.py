f940a3809a5bc2f4e913f9703d84283ff8bc378e
ElsSweep <e.sweep@tudelft.nl>
2018-11-07 13:40:32 +0100

    test imported into test and more

--- script.py
+++ script.py
@@ -1,1 +1,7 @@
 print('hello github')
+
+from test import circumference, surface_area
+
+print("the circumference of the circle is", circumference(5))
+
+print("The surface area of the circle is", surface_area(5))
--- test.py
+++ test.py
@@ -1,18 +1,21 @@
-print("hello world")
+#print("hello world")
 
 import numpy as np
 
 p = 5
-print("The radius of the circle is", p)
+#print("The radius of the circle is", p)
 
 def circumference(r):
     """This function calculates the circumference of a circle"""
     return 2*np.pi*r
 
-print("the circumference of the circle is", circumference(p))
+#print("the circumference of the circle is", circumference(p))
 
 def surface_area(r):
     """ this is a test to see how to combine two versions """
     return np.pi*r**2
 
-print("The surface area of the circle is", surface_area(p))
+#print("The surface area of the circle is", surface_area(p))
+
+if __name__ == "__main__":
+    print("Hello world")
